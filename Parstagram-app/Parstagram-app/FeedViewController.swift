a95316a4ccff7fb2093775a78d2b3f046db92b91
Luciano Scarpaci <Scarpacii.luciano@gmail.com>
2022-09-30 13:28:42 -0400

    Finished Parstagram Queries

--- Parstagram-app/Parstagram-app/FeedViewController.swift
+++ Parstagram-app/Parstagram-app/FeedViewController.swift
@@ -6,16 +6,63 @@
 //
 
 import UIKit
+import Parse
+import AlamofireImage
 
-class FeedViewController: UIViewController {
+class FeedViewController: UIViewController, UITableViewDelegate,
+                          UITableViewDataSource {
 
+    @IBOutlet weak var tableView: UITableView!
+    // create empty array object of PFObject
+    var posts = [PFObject]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        //make the query
+        let query = PFQuery(className:"Posts")
+        query.includeKey("author")
+        query.limit = 20
+        
+        query.findObjectsInBackground { (posts, error) in
+            if posts != nil {
+                self.posts = posts!
+                self.tableView.reloadData()
+            }
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return posts.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier:   "PostCell") as! PostCell
+        
+        let post = posts[indexPath.row]
+        let user = post["author"] as! PFUser
+        cell.commentLabel.text = post["caption"] as! String
+        
+        let imageFile = post["image"] as! PFFileObject
+        let urlString = imageFile.url!
+        let url = URL(string: urlString)!
+        
+        cell.posterView.af.setImage(withURL: url)
+        
+        
+        
+        return cell
+        
+        
+    }
     /*
     // MARK: - Navigation
 
