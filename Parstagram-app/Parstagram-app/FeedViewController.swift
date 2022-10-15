82ba06669b43db4daefc2c0085a54d467a25f5bd
Luciano Scarpaci <Scarpacii.luciano@gmail.com>
2022-10-14 23:17:10 -0400

    Finished User can view comments on a post

--- Parstagram-app/Parstagram-app/FeedViewController.swift
+++ Parstagram-app/Parstagram-app/FeedViewController.swift
@@ -72,6 +72,26 @@ class FeedViewController: UIViewController, UITableViewDelegate,
         
         
     }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let post = posts[indexPath.row]
+        
+        let comment = PFObject(className: "Comments")
+        comment["text"] = "This is a random comment"
+        comment["post"] = post
+        comment["author"] = PFUser.current()!
+        
+        post.add(comment, forKey: "comments")
+        
+        post.saveInBackground { (success, error) in
+            if success {
+                print("comment saved")
+            }
+            else {
+                print("error saving comment")
+            }
+        }
+    }
     /*
     // MARK: - Navigation
 
